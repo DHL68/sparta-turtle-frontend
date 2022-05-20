e777f73e1063749a8a5370b7c69fe52058d40447
DHL68 <ldh1255@naver.com>
2022-05-21 02:04:59 +0900

    Update : 6강 게시글 수정 기능 추가 #1

--- article_detail.js
+++ article_detail.js
@@ -17,4 +17,34 @@ async function loadArticle(article_id) {
     time.innerText = article.time
 }
 
+// ui 만 바꿔주는 것이기 때문에 async 를 넣지 않는다.
+function updateMode() {
+
+    const title = document.getElementById("title")
+    const content = document.getElementById("content")
+    // 엘리멘트 하이드 가리는 기능
+    title.style.visibility = "hidden"
+    content.style.visibility = "hidden"
+
+    const input_title = document.createElement("textarea")
+    input_title.setAttribute("id", "input_title")
+    input_title.innerText = title.innerHTML
+
+    const input_content = document.createElement("textarea")
+    input_content.setAttribute("id", "input_content")
+    // content.innerHTML 으로 내용을 불러옴으로써 수정하기 클릭시 내용이 포함되어 input 변경
+    input_content.innerText = content.innerHTML
+    // 10줄 제한
+    input_content.row = 10
+
+    const body = document.body
+    body.insertBefore(input_title, title)
+    body.insertBefore(input_content, content)
+
+
+}
+
+
+
+
 loadArticle(article_id);
